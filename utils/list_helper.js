e3f1f902ac155c0fc55311e768075d297037560f
Petar Petrov <ppetrovbusiness@gmail.com>
2023-05-21 12:42:58 +0300

    add lodash, solve 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
     return 1
 }
@@ -26,8 +28,28 @@ const favoriteBlog = (blogs) => {
     return favoriteBlog
 }
 
+const mostBlogs = (blogs) => {
+    if(blogs.length === 0) {
+        return undefined
+    }
+    
+    const counted = _.countBy(blogs, (blog) => blog.author)
+
+    const authoredBlogs = _.map(counted, (value, key) => {
+        return {
+            author: key,
+            blogs: value
+        }
+    })
+
+    const mostBlogsAuthor = _.maxBy(authoredBlogs,blog => blog.blogs)
+
+    return mostBlogsAuthor
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
+    mostBlogs,
 }
